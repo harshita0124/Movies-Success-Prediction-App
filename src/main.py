bca1c71eb0c83df370dd774ed1db760cee975395
harshita0124 <harshita.gupta3021@gmail.com>
2025-11-30 17:57:21 +0530

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+#======================================================================================
+#                                   Main Page UI
+#======================================================================================
+
+
+def main_config ():
+    st.title("🎬 Movie Box Office Success Prediction")
+    st.warning("Don't wait for the news to know about the movie success. Do it yourself by predicting here!!👇.")
+    st.markdown("---")
+    st.video("video/movie_video.mp4")
